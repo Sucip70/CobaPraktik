8d142ee0792306a18e9e9b0f04f357552735b2c1
Sucip70 <45531730+Sucip70@users.noreply.github.com>
2020-09-25 15:07:13 +0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class Test{ 
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in); 
+  
+        int ok = key.nextInt(); 
+        int o = key.nextInt(); 
+        //System.out.println((o+k)); 
+    } 
+} 
